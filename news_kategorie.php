f4f3f21a99d43c4d31cc0c31df01c1aebafdcdd5
wojteklapacz <wojtek-info@wp.pl>
2018-11-16 22:07:54 +0100

    Z edycją kategorii newsów

--- news_kategorie.php
+++ news_kategorie.php
@@ -25,7 +25,11 @@ if($akcja=="nowy"){
 } elseif($akcja=="dodaj"){
 	//zapis nowego rekordu do tabeli
 	$rok = date("Y");
-	$lokalizacja = "./zasoby/".$rok."/news_kategorie/news_kategoria.jpg";
+	$sql = "SELECT * FROM kategorie_news ORDER BY `katnews_id` DESC LIMIT 1";
+	$query = mysqli_query($link, $sql);
+	$dane = mysqli_fetch_array($query);
+	$id = $dane['katnews_id'] + 1;
+	$lokalizacja = "./zasoby/".$rok."/news_kategorie/news_kategoria".$id.".jpg";
 	if ($_FILES['obraz']['type'] != 'image/jpeg'){
 		echo'To nie jest obraz .jpg!';
 	} else {
@@ -46,10 +50,35 @@ if($akcja=="nowy"){
 	
 } elseif($akcja=="edycja"){
 	//formularz z danymi do edycji
+	$sql = "SELECT * FROM `kategorie_news` WHERE `katnews_id`=".$_GET['id'];
+	$query = mysqli_query($link, $sql);
+	$rekord = mysqli_fetch_array($query);
+	
+	echo'
+		<form enctype="multipart/form-data" 
+			action="index.php?akcja=edytuj&id='.$_GET['id'].'" method="post">		
+		<label>Nazwa kategorii newsów:
+		<input name="nazwa" type="text" size="30"
+			value="'.$rekord['katnews_nazwa'].'">
+		</label><br>
+		<label>Kolejność:
+		<input name="kolejnosc" type="text" size="3"
+			value="'.$_GET['id'].'">
+		</label><br>
+			<label>Obraz kategorii:
+				<img src="'.$rekord['katnews_obraz'].'">
+			</label><br>
+		<input name="obraz" type="file"><br><br>
+		<input type="submit" value="Zapisz zmiany">		
+		</form>
+		';
 	
 } elseif($akcja=="edytuj"){
 	//aktualizacja rekordu w tabeli
-	
+	$sql = 'UPDATE `kategorie_news` SET `katnews_nazwa`= "'.$_POST['nazwa'].'",
+	`katnews_kolejnosc`='.$_POST['kolejnosc'].'
+	WHERE `katnews_id`='.$_GET['id'];
+	$sql;
 } elseif($akcja=="usun"){
 	//skasowanie rekordu w tabeli
 	$sql="DELETE FROM `kategorie_news` WHERE `katnews_id`=".$_GET['id'];
